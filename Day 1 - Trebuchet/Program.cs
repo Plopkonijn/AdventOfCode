b8cab198723f858ec63f1f6926fd5c4d1e833b03
Jorijn Lamberink <Jorijn@geostruct.com>
2023-12-09 13:41:00 +0100

    created solution for part one

--- Day 1 - Trebuchet/Program.cs
+++ Day 1 - Trebuchet/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var sumOfCalibrationValues = File.ReadLines("input.txt")
+                     .Select(GetCalibrationValue)
+                     .Sum();
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine($"The sum of all of the calibration values is {sumOfCalibrationValues}");
+
+int GetCalibrationValue(string line)
+{
+	var calibrationString = GetCalibrationString(line);
+	return int.Parse(calibrationString);
+}
+
+string GetCalibrationString(string line)
+{
+	var firstDigit = line.First(char.IsNumber);
+	var secondDigit = line.Last(char.IsNumber);
+	return string.Concat(firstDigit, secondDigit);
+}
