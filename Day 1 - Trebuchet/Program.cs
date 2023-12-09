72f2ed61a3ccefe33919aea94a358b92bd451f5a
Jorijn Lamberink <Jorijn@geostruct.com>
2023-12-09 14:55:18 +0100

    improved solution for part two

--- Day 1 - Trebuchet/Program.cs
+++ Day 1 - Trebuchet/Program.cs
@@ -1,18 +1,41 @@
-﻿var sumOfCalibrationValues = File.ReadLines("input.txt")
-                     .Select(GetCalibrationValue)
-                     .Sum();
+﻿using System.Text.RegularExpressions;
+
+int sumOfCalibrationValues =
+	File.ReadLines("input.txt")
+	    .Select(GetCalibrationValue)
+	    .Sum();
 
 Console.WriteLine($"The sum of all of the calibration values is {sumOfCalibrationValues}");
 
 int GetCalibrationValue(string line)
 {
-	var calibrationString = GetCalibrationString(line);
+	string calibrationString = GetCalibrationString(line);
 	return int.Parse(calibrationString);
 }
 
 string GetCalibrationString(string line)
 {
-	var firstDigit = line.First(char.IsNumber);
-	var secondDigit = line.Last(char.IsNumber);
-	return string.Concat(firstDigit, secondDigit);
+	Match firstMatch = Regex.Match(line, @"(\d|one|two|three|four|five|six|seven|eight|nine)");
+	Match lastMatch = Regex.Match(line, @"(\d|one|two|three|four|five|six|seven|eight|nine)", RegexOptions.RightToLeft);
+
+	char firstDigit = ConvertDigit(firstMatch.Value);
+	char lastDigit = ConvertDigit(lastMatch.Value);
+	return string.Concat(firstDigit, lastDigit);
+}
+
+char ConvertDigit(string digit)
+{
+	return digit switch
+	{
+		"one" => '1',
+		"two" => '2',
+		"three" => '3',
+		"four" => '4',
+		"five" => '5',
+		"six" => '6',
+		"seven" => '7',
+		"eight" => '8',
+		"nine" => '9',
+		_ => digit.Single()
+	};
 }
