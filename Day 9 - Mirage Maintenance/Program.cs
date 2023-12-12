f7bb8dda45abfed16c1e0a6ae65f9aa14650ad55
Jorijn Lamberink <Jorijn@geostruct.com>
2023-12-12 18:37:18 +0100

    implemented input parsing

--- Day 9 - Mirage Maintenance/Program.cs
+++ Day 9 - Mirage Maintenance/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System.Text.RegularExpressions;
+
+string text = """
+              0 3 6 9 12 15
+              1 3 6 10 15 21
+              10 13 16 21 30 45
+              """;
+//text = File.ReadAllText("input.txt");
+
+List<Sequence> sequences = Sequence.ParseSequences(text).ToList();
+var answerPartOne = "NaN";
+Console.WriteLine(answerPartOne);
+
+internal class Sequence
+{
+	private readonly int[] _values;
+
+	private Sequence(IEnumerable<int> values)
+	{
+		_values = values.ToArray();
+	}
+
+	public static IEnumerable<Sequence> ParseSequences(string text)
+	{
+		return Regex.Matches(text, @"((?<values>\d+)[ ]*)+")
+		            .Select(match => match.Groups["values"]
+		                                  .Captures
+		                                  .Select(capture => int.Parse(capture.Value)))
+		            .Select(values => new Sequence(values));
+	}
+}
