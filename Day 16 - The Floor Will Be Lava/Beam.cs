76e5f23023e336632d7efce7e2f284f95918b7e0
Jorijn Lamberink <Jorijn@geostruct.com>
2023-12-16 18:06:05 +0100

    added Beam

---
+++ Day 16 - The Floor Will Be Lava/Beam.cs
@@ -0,0 +1,35 @@
+namespace TheFloorWillBeLava;
+
+internal record Beam
+{
+	private readonly int _hashCode;
+
+	public Beam(int positionX, int positionY, int directionX, int directionY)
+	{
+		_hashCode = HashCode.Combine(positionX, positionY, directionX, directionY);
+		PositionX = positionX;
+		PositionY = positionY;
+		DirectionX = directionX;
+		DirectionY = directionY;
+	}
+
+	public int PositionX { get; private set; }
+	public int PositionY { get; private set; }
+	public int DirectionX { get; private set; }
+	public int DirectionY { get; private set; }
+
+	public void Move()
+	{
+		PositionX += DirectionX;
+		PositionY += DirectionY;
+	}
+
+	public void ChangeDirection(int directionX, int directionY)
+	{
+		DirectionX = directionX;
+		DirectionY = directionY;
+	}
+
+	public override int GetHashCode() => _hashCode;
+
+}
