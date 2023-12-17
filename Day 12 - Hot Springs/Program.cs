a47593ac8f05d96003aeb331a786cd764c396e35
Jorijn Lamberink <Jorijn@geostruct.com>
2023-12-17 14:44:24 +0100

    implemented solution part one

--- Day 12 - Hot Springs/Program.cs
+++ Day 12 - Hot Springs/Program.cs
@@ -1,3 +1,53 @@
-﻿var springRecords = File.ReadLines("example.txt")
-                        .Select(SpringRecord.Parse)
-                        .ToList();
+﻿using System.Diagnostics;
+
+List<SpringRecord> springRecords = File.ReadLines("input.txt")
+                                       .Select(SpringRecord.Parse)
+                                       .ToList();
+
+var timeStamp = Stopwatch.GetTimestamp();
+int answerPartOne = springRecords.Select(record => record.CountPossibleArrangements()).Sum();
+var elapsedTime = Stopwatch.GetElapsedTime(timeStamp);
+Console.WriteLine($"Answer to part one: {answerPartOne}, in {elapsedTime.Milliseconds} ms");
+
+
+
+Console.WriteLine();
+/*   string length 	: 14
+ *   minimum size  	: (1+1+3) + (3 - 1) = 7
+ *   wiggle room	: 14 - 7 + 1 = 8
+ *
+ *		.	?	?	.	.	?	?	.	.	.	?	#	#	.
+ *  *  [1	1	1	1	1	1	1	1]
+ *	1  [0   1	2	2	2	3	4	4]
+ *	1		   [0   0	0	2	4	4	4	4]
+ *	3				   [0   0   0	0	0	0	4	4]
+ *
+ *	string length	: 12
+ *  minimum size	: (3 + 2 + 1 ) + (3 - 1) = 8
+ *
+ *		?	#	#	#	?	?	?	?	?	?	?	?
+ *  *  [1   1   1   1   1]
+ *	3  [0   1   1	1	1]
+ *	2				   [0   1	2	3	4]
+ *	1							   [0   1   3	6	10]
+ *
+ *	string length	: 15
+ *  minimum size	: (1+3+1+6) + (4 - 1) = 14
+ *		?	#	?	#	?	#	?	#	?	#	?	#	?	#	?
+ *  *  [1	1]
+ *	1  [0   1]
+ *	3          [0	1]
+ *	1						   [0	1]
+ *	6								   [0   1]
+ *
+ *	string length	: 19
+ *	minimum size	: (1 + 6 + 4) + (3 - 1) = 13
+ *  wiggle room		: 19 - 13 + 1 = 7
+ *
+ *		?	?	?	?	.	#	#	#	#	#	#	.	.	#	#	#	#	#	.
+ *	*  [1	1	1	1	1	1	1]
+ *	1  [1	2	3	4	4	4	4]
+ *	6		   [0	0	0	4	4	4	4]
+ *	5									   [0	0	0	0	4	4	4]
+ *
+ */
--- Day 12 - Hot Springs/SpringRecord.cs
+++ Day 12 - Hot Springs/SpringRecord.cs
@@ -10,4 +10,33 @@ public record SpringRecord(string DamagedRecord, int[] SpringGroups)
 		                          .ToArray();
 		return new SpringRecord(damagedRecord, springGroups);
 	}
+
+	public int CountPossibleArrangements()
+	{
+		var generatedRecords = new List<string> { DamagedRecord };
+		var indices = DamagedRecord.Select((c, i) => (c, i)).Where(t => t.c is '?').Select(t => t.i).ToArray();
+		foreach(int i in indices)
+		{
+			int count = generatedRecords.Count;
+			for (int j = 0; j < count; j++)
+			{
+				var record = generatedRecords[j];
+				if (record[i] is '?')
+				{
+					var chars = record.ToCharArray();
+					chars[i] = '#';
+					generatedRecords[j] = new string(chars);
+					chars[i] = '.';
+					generatedRecords.Add(new string(chars));
+				}
+			}
+		}
+
+		var pattern = string.Join("[.]+",SpringGroups.Select(i => $"[#]{{{i}}}"));
+		var regex = new Regex($"^[^#]*{pattern}[^#]*$");
+		var matches =  generatedRecords.Select(cc => new string(cc))
+		                .Where(s => regex.IsMatch(s))
+		                .ToArray();
+		return matches.Length;
+	}
 }
