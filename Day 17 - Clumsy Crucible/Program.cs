ebe932e65961cbe8df6e390dd523a7ca543cacde
Jorijn Lamberink <Jorijn@geostruct.com>
2023-12-17 22:32:46 +0100

    added class City

--- Day 17 - Clumsy Crucible/Program.cs
+++ Day 17 - Clumsy Crucible/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var text = File.ReadAllLines("example.txt");
+var city = new City(text);
 
-Console.WriteLine("Hello, World!");
+int answerPartOne = 0;
+Console.WriteLine(answerPartOne);
+
+class City
+{
+	private readonly string[] _text;
+	public int Width { get; }
+	public int Height { get; }
+
+	public City(string[] text)
+	{
+		_text = text;
+		Width = text[0].Length;
+		Height = text.Length;
+	}
+}
