71271b702e4b9f095a6f8ee492326cf2bb725238
Jorijn Lamberink <Jorijn@geostruct.com>
2023-12-18 23:53:57 +0100

    implemented solution part one

--- Day 18 - Lavaduct Lagoon/Day 18 - Lavaduct Lagoon/DigInstruction.cs
+++ Day 18 - Lavaduct Lagoon/Day 18 - Lavaduct Lagoon/DigInstruction.cs
@@ -20,4 +20,27 @@ internal record DigInstruction(DigDirection Direction, int Distance, Color Color
 		Color color = ColorTranslator.FromHtml(match.Groups["color"].Value);
 		return new DigInstruction(direction, distance, color);
 	}
+
+	public IEnumerable<(int x, int y)> DigTrenches(int x, int y)
+	{
+		(int dx, int dy) = GetDirectionTuple();
+		for (int i = 0; i < Distance; i++)
+		{
+			x += dx;
+			y += dy;
+			yield return (x, y);
+		}
+	}
+
+	private (int x, int y) GetDirectionTuple()
+	{
+		return Direction switch
+		{
+			DigDirection.Up => (0, +1),
+			DigDirection.Down => (0, -1),
+			DigDirection.Left => (-1, 0),
+			DigDirection.Right => (+1, 0),
+			_ => throw new ArgumentOutOfRangeException()
+		};
+	}
 }
