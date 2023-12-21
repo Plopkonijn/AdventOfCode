2f061646245900fc933b7bc41a7c1acd49411050
Jorijn Lamberink <Jorijn@geostruct.com>
2023-12-21 22:12:15 +0100

    added parsing of input text

--- Day 13 - Point of Incidence/Program.cs
+++ Day 13 - Point of Incidence/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿IEnumerable<string> text = File.ReadLines("example.txt");
+ParsePatterns(text);
+Console.WriteLine();
 
-Console.WriteLine("Hello, World!");
+void ParsePatterns(IEnumerable<string> enumerable)
+{
+	var patterns = new List<List<string>>();
+	var currentPattern = new List<string>();
+	foreach (string line in enumerable)
+	{
+	
+		if (string.IsNullOrWhiteSpace(line))
+		{
+			patterns.Add(currentPattern);
+			currentPattern = new List<string>();
+			continue;
+		}
+		currentPattern.Add(line);
+	}
+
+	if (currentPattern.Any())
+	{
+		patterns.Add(currentPattern);
+	}
+}
