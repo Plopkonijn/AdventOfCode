b2bb115d030fe7390a2af3013f0cddb4a7cf339a
Jorijn Lamberink <Jorijn@geostruct.com>
2023-12-23 17:07:24 +0100

    attempt at solution part two

--- Day 14 - Parabolic Reflector Dish/Platform.cs
+++ Day 14 - Parabolic Reflector Dish/Platform.cs
@@ -1,9 +1,10 @@
+using System.Collections;
 using System.Text;
 
 class Platform
 {
 	private HashSet<Location> _cubeRocks = new();
-	private HashSet<Location> _roundRocks = new();
+	private List<Location> _roundRocks = new();
 	private int _width;
 	private int _height;
 
@@ -24,25 +25,96 @@ class Platform
 		}
 	}
 
-	public void RollNorth()
+	private int HashRoundRocks()
 	{
-		var _rollingRocks = new PriorityQueue<Location, int>(_roundRocks.Select(location => (location, location.Y)));
-		while (_rollingRocks.TryDequeue(out var currentLocation, out int y))
+		return _roundRocks.Select(location => location.GetHashCode())
+		                  .Aggregate(HashCode.Combine);
+	}
+	
+	
+	public (int remainingCycles, int cycleLength) RollDirections(Direction[] directions, int cycles)
+	{
+		var remainingCycles = cycles;
+		var cycleLength = 0;
+		var cyclesDone = 0;
+		var hashes = new HashSet<string>();
+		var locationComparers = CreateLocationComparers(directions);
+		while (remainingCycles-- > 0)
 		{
-			Location? nextLocation = null;
-			var location = currentLocation with { Y = y - 1 };
-			while (location.Y >= 0 && !_cubeRocks.Contains(location) && !_roundRocks.Contains(location))
+			cyclesDone++;
+			if (!hashes.Add(ToString()))
 			{
-				nextLocation = location;
-				location = location with { Y = location.Y-1 };
+				break;
 			}
-
-			if (nextLocation is {} updatedLocation)
-			{
-				_roundRocks.Remove(currentLocation);
-				_roundRocks.Add(updatedLocation);
-			}
+			RollCycle(directions,locationComparers);
 		}
+
+		cycleLength = cyclesDone;
+		remainingCycles = cycles % cycleLength;
+		while (remainingCycles-- > 0)
+		{
+			cyclesDone++;
+			RollCycle(directions,locationComparers);
+		}
+
+		return (cyclesDone, cycleLength);
+	}
+
+	private Comparison<Location>[] CreateLocationComparers(Direction[] directions)
+	{
+		return directions.Select(direction => direction switch
+		{
+			(< 0, _) => new Comparison<Location>((a, b) => a.X.CompareTo(b.X)),
+			(> 0, _) => new Comparison<Location>((a, b) => (_width - a.X).CompareTo(_width - b.X)),
+			(_, < 0) => new Comparison<Location>((a, b) => a.Y.CompareTo(b.Y)),
+			(_, > 0) => new Comparison<Location>((a, b) => (_height - a.Y).CompareTo(_height - b.Y)),
+			_ => throw new InvalidOperationException()
+		})
+		.ToArray();
+	}
+
+	private void RollCycle(Direction[] directions, Comparison<Location>[] locationComparers)
+	{
+		for (int index = 0; index < directions.Length; index++)
+		{
+			Direction direction = directions[index];
+			var locationComparer = locationComparers[index];
+			RollDirection(direction,locationComparer);
+		}
+	}
+
+	private void RollDirection(Direction direction, Comparison<Location> locationComparer)
+	{
+		_roundRocks.Sort(locationComparer);
+		for (int i = 0; i < _roundRocks.Count; i++)
+		{
+			var currentLocation = _roundRocks[i];
+			if (FindNextLocation(direction, currentLocation) is { } nextLocation)
+			{
+				_roundRocks[i] = nextLocation;
+			};
+		}
+	}
+
+	private Location? FindNextLocation(Direction direction, Location currentLocation)
+	{
+		Location? nextLocation = null;
+		var location = currentLocation.Move(direction);
+		while (IsInBounds(location) && !_cubeRocks.Contains(location) && !_roundRocks.Contains(location))
+		{
+			nextLocation = location;
+			location = location.Move(direction);
+		}
+
+		return nextLocation;
+	}
+
+	private bool IsInBounds(Location location)
+	{
+		return location.X >= 0
+		    && location.X < _width
+		    && location.Y >= 0
+		    && location.Y < _height;
 	}
 
 	public override string ToString()
