328d0752d4748c0131c549bdf068a3a31e4604f6
Jorijn Lamberink <Jorijn@geostruct.com>
2023-12-23 18:50:36 +0100

    started on cli

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using Year2023.Day2;
+
+namespace AdventOfCode;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		if (!TrySelectProblem(out var problemSelection))
+			return;
+		Console.WriteLine($"You've selected : {problemSelection}");
+	}
+
+	private static bool TrySelectProblem([NotNullWhen(true)] out ProblemSelection? problemSelection)
+	{
+		problemSelection = null;
+		Console.Write("Year: ");
+		if (!int.TryParse(Console.ReadLine(), out var year))
+		{
+			return false;
+		}
+
+		Console.Write("Day: ");
+		if (!int.TryParse(Console.ReadLine(), out var day))
+		{
+			return false;
+		}
+
+		Console.Write("Part: ");
+		if (!int.TryParse(Console.ReadLine(), out var part))
+		{
+			return false;
+		}
+
+		problemSelection = new ProblemSelection(year, day, part);
+		return true;
+	}
+}
