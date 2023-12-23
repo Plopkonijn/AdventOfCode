710c466185d006f739eadbe8f635f0d1fd6006de
Jorijn Lamberink <Jorijn@geostruct.com>
2023-12-23 18:17:05 +0100

    refactored day one

---
+++ Year 2023/Day 1 - Trebuchet/CalibrationLine.cs
@@ -0,0 +1,49 @@
+using System.Text.RegularExpressions;
+
+namespace Year2023.Trebuchet;
+
+internal partial record CalibrationLine(string Value)
+{
+	public int GetCalibrationValue()
+	{
+		int firstDigit = int.Parse(Value.First(char.IsNumber).ToString());
+		int lastDigit = int.Parse(Value.Last(char.IsNumber).ToString());
+		return CombineDigits(firstDigit, lastDigit);
+	}
+
+	public int GetSpelledCalibrationValue()
+	{
+		int firstDigit = ParseDigit(MatchFirstDigit().Match(Value).Value);
+		int lastDigit = ParseDigit(MatchLastDigit().Match(Value).Value);
+		return CombineDigits(firstDigit, lastDigit);
+	}
+
+	private static int CombineDigits(int firstDigit, int lastDigit)
+	{
+		return 10 * firstDigit + lastDigit;
+	}
+
+	private static int ParseDigit(string digit)
+	{
+		return digit switch
+		{
+			"one" => 1,
+			"two" => 2,
+			"three" => 3,
+			"four" => 4,
+			"five" => 5,
+			"six" => 6,
+			"seven" => 7,
+			"eight" => 8,
+			"nine" => 9,
+			_ => int.Parse(digit)
+		};
+	}
+
+	private const string DIGIT_PATTERN = @"(\d|one|two|three|four|five|six|seven|eight|nine)";
+	[GeneratedRegex(DIGIT_PATTERN)]
+	private static partial Regex MatchFirstDigit();
+
+	[GeneratedRegex(DIGIT_PATTERN, RegexOptions.RightToLeft)]
+	private static partial Regex MatchLastDigit();
+}
--- Year 2023/Day 1 - Trebuchet/Program.cs
+++ Year 2023/Day 1 - Trebuchet/Program.cs
@@ -1,41 +1,11 @@
-﻿using System.Text.RegularExpressions;
+﻿using Year2023.Trebuchet;
 
-int sumOfCalibrationValues =
-	File.ReadLines("input.txt")
-	    .Select(GetCalibrationValue)
-	    .Sum();
+List<CalibrationLine> calibrationLines = File.ReadLines("input.txt")
+                                             .Select(line => new CalibrationLine(line))
+                                             .ToList();
 
-Console.WriteLine($"The sum of all of the calibration values is {sumOfCalibrationValues}");
+int answerPartOne = calibrationLines.Sum(line => line.GetCalibrationValue());
+Console.WriteLine(answerPartOne);
 
-int GetCalibrationValue(string line)
-{
-	string calibrationString = GetCalibrationString(line);
-	return int.Parse(calibrationString);
-}
-
-string GetCalibrationString(string line)
-{
-	Match firstMatch = Regex.Match(line, @"(\d|one|two|three|four|five|six|seven|eight|nine)");
-	Match lastMatch = Regex.Match(line, @"(\d|one|two|three|four|five|six|seven|eight|nine)", RegexOptions.RightToLeft);
-
-	char firstDigit = ConvertDigit(firstMatch.Value);
-	char lastDigit = ConvertDigit(lastMatch.Value);
-	return string.Concat(firstDigit, lastDigit);
-}
-
-char ConvertDigit(string digit)
-{
-	return digit switch
-	{
-		"one" => '1',
-		"two" => '2',
-		"three" => '3',
-		"four" => '4',
-		"five" => '5',
-		"six" => '6',
-		"seven" => '7',
-		"eight" => '8',
-		"nine" => '9',
-		_ => digit.Single()
-	};
-}
+int answerPartTwo = calibrationLines.Sum(line => line.GetSpelledCalibrationValue());
+Console.WriteLine(answerPartTwo);
