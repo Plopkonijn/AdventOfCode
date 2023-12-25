15eab9693368e6d51fd5d2922847bf3246e09f74
Jorijn Lamberink <Jorijn@geostruct.com>
2023-12-25 21:19:35 +0100

    added project day 25

---
+++ Year 2023/Day 25 - Snowverload/SnowverloadSolver.cs
@@ -0,0 +1,21 @@
+﻿using Application;
+
+namespace Year2023.Day25;
+
+public sealed class SnowverloadSolver : ISolver
+{
+	public SnowverloadSolver(string[] args)
+	{
+		throw new NotImplementedException();
+	}
+
+	public long PartOne()
+	{
+		throw new NotImplementedException();
+	}
+
+	public long PartTwo()
+	{
+		throw new NotImplementedException();
+	}
+}
