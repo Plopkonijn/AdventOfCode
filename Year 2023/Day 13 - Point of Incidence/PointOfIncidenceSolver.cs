f9e4a790eed54cbe91f50a22b50a7a907e92e32e
Jorijn Lamberink <Jorijn@geostruct.com>
2023-12-25 18:24:57 +0100

    find smudge index as well

--- Year 2023/Day 13 - Point of Incidence/PointOfIncidenceSolver.cs
+++ Year 2023/Day 13 - Point of Incidence/PointOfIncidenceSolver.cs
@@ -1,3 +1,5 @@
+using System.Data;
+using System.IO.Compression;
 using Application;
 using Common;
 
@@ -25,14 +27,24 @@ public sealed class PointOfIncidenceSolver : ISolver
 
 	public long PartTwo()
 	{
-		int verticalTotal = _patterns.Select((pattern, i) => (GetFirstVerticalMirrorIndex(pattern, 1), i))
-		                             .ToArray()
-		                             .Sum(t => t.Item1);
-		int horizontalTotal = _patterns.Select((pattern, i) => (GetFirstHorizontalMirrorIndex(pattern, 1), i))
-		                               .ToArray()
-		                               .Sum(t => t.Item1);
-		int result = horizontalTotal * 100 + verticalTotal;
-		return result;
+		foreach (List<string> pattern in _patterns)
+		{
+			var vertical = GetFirstVerticalMirrorIndex(pattern);
+			var horizontal = GetFirstHorizontalMirrorIndex(pattern);
+			var verticalSmudge = GetFirstVerticalSmudgedMirrorIndex(pattern);
+			var horizontalSmudge = GetFirstHorizontalSmudgedMirrorIndex(pattern);
+		}
+
+		return default;
+
+		// int verticalTotal = _patterns.Select((pattern, i) => (GetFirstVerticalMirrorIndex(pattern, 1), i))
+		//                              .ToArray()
+		//                              .Sum(t => t.Item1);
+		// int horizontalTotal = _patterns.Select((pattern, i) => (GetFirstHorizontalMirrorIndex(pattern, 1), i))
+		//                                .ToArray()
+		//                                .Sum(t => t.Item1);
+		// int result = horizontalTotal * 100 + verticalTotal;
+		// return result;
 	}
 
 	private int GetFirstVerticalMirrorIndex(List<string> pattern)
@@ -48,24 +60,36 @@ public sealed class PointOfIncidenceSolver : ISolver
 			               , 0);
 	}
 
-	private int GetFirstVerticalMirrorIndex(List<string> pattern, int smudges)
+	record struct SmudgeMirrorMatch(int MirrorIndex, int? SmudgeColumn, int? SmudgeRow);
+
+	private SmudgeMirrorMatch? GetFirstVerticalSmudgedMirrorIndex(List<string> pattern)
 	{
 		var result = Enumerable.Range(1, pattern[0].Length - 1)
-		                 .FirstOrDefault(i =>
-		                 {
-			                 var differences = 0;
-			                 foreach (string line in pattern)
-			                 {
-				                 if (differences > smudges)
-					                 break;
-				                 differences += line.Take(i)
-				                                    .Reverse()
-				                                    .Zip(line.Skip(i))
-				                                    .Count(t => t.First != t.Second);
-			                 }
+		                       .Select(i =>
+		                       {
+			                       foreach ((string line, int row) in pattern.Select((s, i) => (s, i)))
+			                       {
+				                       var smudges = line.Take(i)
+				                                         .Reverse()
+				                                         .Zip(line.Skip(i))
+				                                         .Select((c, column) => (c, column))
+				                                         .Where(t => t.c.First != t.c.Second);
+				                       switch (smudges.Count())
+				                       {
+					                       case > 1:
+						                       continue;
+					                       case 0:
+						                       return new SmudgeMirrorMatch(i, null, null);
+					                       case 1:
+						                       ((char First, char Second) c, int column) smudge = smudges.SingleOrDefault();
+						                       return new SmudgeMirrorMatch(i, smudge
+						                                                              .column, row);
+				                       }
+			                       }
 
-			                 return differences <= smudges;
-		                 }, 0);
+			                       return (SmudgeMirrorMatch?)null;
+		                       })
+		                       .FirstOrDefault(o => o != null,null);
 		return result;
 	}
 
@@ -82,26 +106,30 @@ public sealed class PointOfIncidenceSolver : ISolver
 			               , 0);
 	}
 
-	private int GetFirstHorizontalMirrorIndex(List<string> pattern, int smudges)
+	private SmudgeMirrorMatch? GetFirstHorizontalSmudgedMirrorIndex(List<string> pattern)
 	{
 		var result = Enumerable.Range(1, pattern.Count - 1)
-		                 .FirstOrDefault(i =>
-		                 {
-			                 var differences = 0;
-			                 var tuples = pattern.Take(i)
-			                                     .Reverse()
-			                                     .Zip(pattern.Skip(i))
-			                                     .SelectMany(t => t.First.Zip(t.Second));
-			                 foreach ((char first, char second) in tuples)
-			                 {
-				                 if (differences > smudges)
-					                 break;
-				                 if (first != second)
-					                 differences++;
-			                 }
-
-			                 return differences <= smudges;
-		                 }, 0);
+		                       .Select(i =>
+		                       {
+			                       var smudges = pattern.Take(i)
+			                                            .Reverse()
+			                                            .Zip(pattern.Skip(i))
+			                                            .Select((s, row) => (s, row))
+			                                            .SelectMany(t1 => t1.s.First.Zip(t1.s.Second, (x, y) => { return (x, y, t1.row); })
+			                                                                .Select((t2, column) => (t2.x, t2.y, column, t2.row)))
+			                                            .Where(t3 => t3.x != t3.y);
+			                       switch (smudges.Count())
+			                       {
+				                       case 0:
+					                       return new SmudgeMirrorMatch(i, null, null);
+				                       case 1:
+					                       (char x, char y, int column, int row) smudge = smudges.SingleOrDefault();
+					                       return new SmudgeMirrorMatch(i, smudge.column, smudge.row);
+				                       default:
+					                       return (SmudgeMirrorMatch?)null;
+			                       }
+		                       })
+		                       .FirstOrDefault(o=>o!=null,null);
 		return result;
 	}
 }
