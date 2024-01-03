46575ce85235db46b87aba635884c8f80368861f
Jorijn Lamberink <Jorijn@geostruct.com>
2024-01-03 17:32:32 +0100

    added project day 15

---
+++ Year 2023/Day 15 - Lens Library/Class1.cs
@@ -0,0 +1,20 @@
+﻿using Application;
+
+namespace Year2023.Day15;
+
+public class LensLibrarySolver : ISolver
+{
+	public LensLibrarySolver(string[] args)
+	{
+	}
+
+	public long PartOne()
+	{
+		throw new NotImplementedException();
+	}
+
+	public long PartTwo()
+	{
+		throw new NotImplementedException();
+	}
+}
