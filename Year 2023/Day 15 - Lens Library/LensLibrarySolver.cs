08d61d1dfa5a3d2e22ace575a34d55be1b331ed3
Jorijn Lamberink <Jorijn@geostruct.com>
2024-01-03 17:46:38 +0100

    implemented solution part one

--- Year 2023/Day 15 - Lens Library/LensLibrarySolver.cs
+++ Year 2023/Day 15 - Lens Library/LensLibrarySolver.cs
@@ -4,17 +4,36 @@ namespace Year2023.Day15;
 
 public class LensLibrarySolver : ISolver
 {
+	private readonly List<string> _initializationSequence;
+
 	public LensLibrarySolver(string[] args)
 	{
+		_initializationSequence = args.SelectMany(s =>
+			                              s.Split(','))
+		                              .ToList();
 	}
 
 	public long PartOne()
 	{
-		throw new NotImplementedException();
+		return _initializationSequence.Sum(Hash);
 	}
 
 	public long PartTwo()
 	{
 		throw new NotImplementedException();
+	}
+
+	private long Hash(string initializationStep)
+	{
+		long hash = 0L;
+
+		foreach (char c in initializationStep)
+		{
+			hash += c;
+			hash *= 17;
+			hash %= 256;
+		}
+
+		return hash;
 	}
 }
