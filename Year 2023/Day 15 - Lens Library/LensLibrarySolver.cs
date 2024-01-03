9c103b9a6d629f531f14b9e7ffab816e9d84eebd
Jorijn Lamberink <Jorijn@geostruct.com>
2024-01-03 18:25:21 +0100

    implemented solution part two

--- Year 2023/Day 15 - Lens Library/LensLibrarySolver.cs
+++ Year 2023/Day 15 - Lens Library/LensLibrarySolver.cs
@@ -1,4 +1,5 @@
-﻿using Application;
+﻿using System.Text.RegularExpressions;
+using Application;
 
 namespace Year2023.Day15;
 
@@ -20,7 +21,46 @@ public class LensLibrarySolver : ISolver
 
 	public long PartTwo()
 	{
-		throw new NotImplementedException();
+		List<(string label, int focalLength)>[] boxes = Enumerable.Range(0, 256)
+		                                                          .Select(_ => new List<(string label, int focalLength)>())
+		                                                          .ToArray();
+		PerformInitializationSteps(boxes);
+		return CalculateFocusingPower(boxes);
+	}
+
+	private static long CalculateFocusingPower(List<(string label, int focalLength)>[] boxes)
+	{
+		return boxes.Select((box, boxNumber) => (box, boxNumber))
+		            .Sum(boxTuple =>
+			            boxTuple.box.Select((lens, slotNumber) => (lens, slotNumber))
+			                    .Sum(lensTuple => (boxTuple.boxNumber + 1) * (lensTuple.slotNumber + 1) * lensTuple.lens.focalLength));
+	}
+
+	private void PerformInitializationSteps(List<(string label, int focalLength)>[] boxes)
+	{
+		foreach (string initializationStep in _initializationSequence)
+		{
+			Match match = Regex.Match(initializationStep, @"(?<label>\w+)(-|(=(?<focalLength>\d+)))");
+			string label = match.Groups["label"].Value;
+			long hash = Hash(label);
+			List<(string label, int focalLength)> box = boxes[hash];
+			int index = box
+				.FindIndex(lens => lens.label == label);
+
+			if (!int.TryParse(match.Groups["focalLength"].Value, out int focalLength))
+			{
+				if (index != -1)
+					box.RemoveAt(index);
+			}
+			else if (index == -1)
+			{
+				box.Add((label, focalLength));
+			}
+			else
+			{
+				box[index] = (label, focalLength);
+			}
+		}
 	}
 
 	private long Hash(string initializationStep)
