af56c1fa9ebafa02683a78c76b9a23019a2eb005
Jorijn Lamberink <Jorijn@geostruct.com>
2024-01-04 22:31:53 +0100

    added project day 20

---
+++ Year 2023/Day 20 - Pulse Propagation/PulsePropagation.cs
@@ -0,0 +1,20 @@
+﻿using Application;
+
+namespace Year2023.Day20;
+
+public sealed class PulsePropagation : ISolver
+{
+	public PulsePropagation(string[] args)
+	{
+	}
+
+	public long PartOne()
+	{
+		throw new NotImplementedException();
+	}
+
+	public long PartTwo()
+	{
+		throw new NotImplementedException();
+	}
+}
