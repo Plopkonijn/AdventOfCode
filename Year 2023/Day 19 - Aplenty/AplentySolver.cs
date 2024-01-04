99baa0661d0b0e00f0386adf5f4de242578c52b5
Jorijn Lamberink <Jorijn@geostruct.com>
2024-01-04 18:09:35 +0100

    added project day 19

---
+++ Year 2023/Day 19 - Aplenty/AplentySolver.cs
@@ -0,0 +1,20 @@
+﻿using Application;
+
+namespace Year2023.Day19;
+
+public class AplentySolver : ISolver
+{
+	public AplentySolver(string[] args)
+	{
+	}
+
+	public long PartOne()
+	{
+		throw new NotImplementedException();
+	}
+
+	public long PartTwo()
+	{
+		throw new NotImplementedException();
+	}
+}
