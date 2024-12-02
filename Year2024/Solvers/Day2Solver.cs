a26f65c84d9bf581cdaa8c14369ab23e91460df4
Plop Konijn <plopkonijn@gmail.com>
2024-12-02 23:44:29 +0100

    cleanup

--- Year2024/Solvers/Day2Solver.cs
+++ Year2024/Solvers/Day2Solver.cs
@@ -25,18 +25,11 @@ public sealed class Day2Solver : AdventOfCodeSolver
 
     private bool IsSafeReport(long[] values, int omitIndex = -1)
     {
-        bool isIncreasing = omitIndex switch
-        {
-            -1 => values[0] <= values[1],
-            0 => values[1] <= values[2],
-            1 => values[0] <= values[2],
-            _ => values[0] <= values[1]
-        };
+        bool isIncreasing = IsIncreasingReport(values, omitIndex);
 
         for (int i = 0; i < values.Length - 1; i++)
         {
             long currentValue = values[i];
-            long nextValue = values[i + 1];
             if (i == omitIndex)
             {
                 if (i == 0)
@@ -46,6 +39,9 @@ public sealed class Day2Solver : AdventOfCodeSolver
 
                 currentValue = values[i - 1];
             }
+
+            long nextValue = values[i + 1];
+
 
             if (i + 1 == omitIndex)
             {
@@ -86,6 +82,17 @@ public sealed class Day2Solver : AdventOfCodeSolver
         }
 
         return true;
+    }
+
+    private static bool IsIncreasingReport(long[] values, int omitIndex)
+    {
+        return omitIndex switch
+        {
+            -1 => values[0] <= values[1],
+            0 => values[1] <= values[2],
+            1 => values[0] <= values[2],
+            _ => values[0] <= values[1]
+        };
     }
 
     public override long SolvePart2(string[] input)
