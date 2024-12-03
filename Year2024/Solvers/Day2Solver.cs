d824ae501921d5121d6b094b07955971fce2bab0
Plop Konijn <plopkonijn@gmail.com>
2024-12-03 18:14:42 +0100

    Solved day3 part2

--- Year2024/Solvers/Day2Solver.cs
+++ Year2024/Solvers/Day2Solver.cs
@@ -142,6 +142,30 @@ public sealed class Day3Solver : AdventOfCodeSolver
 
     public override long SolvePart2(string[] input)
     {
-        throw new NotImplementedException();
+        long total = 0;
+        bool mulEnabled = true;
+        foreach (string line in input)
+        {
+            MatchCollection matches = Regex.Matches(line, @"(?<mul>mul\((?<first>\d{1,3}),(?<second>\d{1,3})\))|(?<do>do\(\))|(?<dont>don\'t\(\))");
+            foreach (Match match in matches.OfType<Match>())
+            {
+                if (match.Groups.TryGetValue("do", out Group? doValue) && doValue.Success)
+                {
+                    mulEnabled = true;
+                }
+                else if (match.Groups.TryGetValue("dont", out Group? dontValue) && dontValue.Success)
+                {
+                    mulEnabled = false;
+                }
+                else if (match.Groups.TryGetValue("mul", out Group? mulValue) && mulValue.Success && mulEnabled)
+                {
+                    long firstOperand = long.Parse(match.Groups["first"].Value);
+                    long secondOperand = long.Parse(match.Groups["second"].Value);
+                    total += firstOperand * secondOperand;
+                }
+            }
+        }
+
+        return total;
     }
 }
