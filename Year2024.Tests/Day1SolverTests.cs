0f0d8272d4905ee756462de6df40d85eae598030
Plop Konijn <plopkonijn@gmail.com>
2024-12-03 20:30:53 +0100

    added puzzle tests day1

--- Year2024.Tests/Day1SolverTests.cs
+++ Year2024.Tests/Day1SolverTests.cs
@@ -2,7 +2,7 @@
 
 namespace Year2024.Tests;
 
-public class Day1SolverTests
+public sealed class Day1SolverTests
 {
     [Fact]
     public void Example1()
@@ -27,6 +27,21 @@ public class Day1SolverTests
     }
 
     [Fact]
+    public void Puzzle1()
+    {
+        // Arrange
+        string[] input = GetPuzzleInput();
+        long expectedOutput = GetPuzzleOutput(1);
+        Day1Solver solver = new(input);
+
+        // Act
+        long actualOutput = solver.SolvePart1();
+
+        // Assert
+        Assert.Equal(expectedOutput, actualOutput);
+    }
+
+    [Fact]
     public void Example2()
     {
         // Arrange
@@ -40,9 +55,43 @@ public class Day1SolverTests
             "3    3",
         ];
         Day1Solver solver = new(input);
+
         // Act
         long result = solver.SolvePart2();
+
         // Assert
         Assert.Equal(31, result);
+    }
+
+    [Fact]
+    public void Puzzle2()
+    {
+        // Arrange
+        string[] input = GetPuzzleInput();
+        long expectedOutput = GetPuzzleOutput(2);
+        Day1Solver solver = new(input);
+
+        // Act
+        long actualOutput = solver.SolvePart2();
+
+        // Assert
+        Assert.Equal(expectedOutput, actualOutput);
+    }
+
+    private static string[] GetPuzzleInput()
+    {
+        string currentDirectory = Directory.GetCurrentDirectory();
+        string directory = Path.Combine(currentDirectory, "data");
+        string filePath = Path.Combine(directory, $"InputDay01.txt");
+        return File.ReadAllLines(filePath);
+    }
+
+    private static long GetPuzzleOutput(int part)
+    {
+        string currentDirectory = Directory.GetCurrentDirectory();
+        string directory = Path.Combine(currentDirectory, "data");
+        string filePath = Path.Combine(directory, $"OutputDay01Part{part}.txt");
+        string outputText = File.ReadAllText(filePath);
+        return long.Parse(outputText);
     }
 }
