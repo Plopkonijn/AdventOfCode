89466fc02adf67b223165e0b10c6878f3c6eb915
Plop Konijn <plopkonijn@gmail.com>
2024-12-03 18:48:51 +0100

    moved types to files

---
+++ Year2024.Tests/Day2SolverTests.cs
@@ -0,0 +1,51 @@
+﻿using Year2024.Solvers;
+
+namespace Year2024.Tests;
+
+public sealed class Day2SolverTests
+{
+    [Fact]
+    public void Example1()
+    {
+        // Arrange
+        string[] input =
+        [
+            "7 6 4 2 1",
+            "1 2 7 8 9",
+            "9 7 6 2 1",
+            "1 3 2 4 5",
+            "8 6 4 4 1",
+            "1 3 6 7 9"
+        ];
+        Day2Solver solver = new(input);
+
+        // Act
+        long result = solver.SolvePart1();
+
+        // Assert
+        Assert.Equal(2, result);
+
+    }
+
+    [Fact]
+    public void Example2()
+    {
+        // Arrange
+        string[] input =
+        [
+            "7 6 4 2 1",
+            "1 2 7 8 9",
+            "9 7 6 2 1",
+            "1 3 2 4 5",
+            "8 6 4 4 1",
+            "1 3 6 7 9"
+        ];
+        Day2Solver solver = new(input);
+
+        // Act
+        long result = solver.SolvePart1();
+
+        // Assert
+        Assert.Equal(2, result);
+    }
+}
