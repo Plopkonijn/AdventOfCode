629cd074675f58442024bd4ea721bd24318558ac
Plop Konijn <plopkonijn@gmail.com>
2024-12-03 18:48:26 +0100

    added tests day2

--- Year2024.Tests/UnitTest1.cs
+++ Year2024.Tests/UnitTest1.cs
@@ -17,10 +17,10 @@ public class Day1SolverTests
             "3    9",
             "3    3",
         ];
-        _ = new Day1Solver(input);
+        Day1Solver solver = new(input);
 
         // Act
-        long result = new Day1Solver(input).SolvePart1();
+        long result = solver.SolvePart1();
 
         // Assert
         Assert.Equal(11, result);
@@ -39,10 +39,58 @@ public class Day1SolverTests
             "3    9",
             "3    3",
         ];
-        _ = new Day1Solver(input);
+        Day1Solver solver = new(input);
         // Act
-        long result = new Day1Solver(input).SolvePart2();
+        long result = solver.SolvePart2();
         // Assert
         Assert.Equal(31, result);
+    }
+}
+
+public sealed class Day2SolverTests
+{
+    [Fact]
+    public void Example1()
+    {
+        // Arrange
+        string[] input =
+        [
+            "7 6 4 2 1",
+            "1 2 7 8 9",
+            "9 7 6 2 1",
+            "1 3 2 4 5",
+            "8 6 4 4 1",
+            "1 3 6 7 9"
+        ];
+        Day2Solver solver = new(input);
+
+        // Act
+        long result = solver.SolvePart1();
+
+        // Assert
+        Assert.Equal(2, result);
+
+    }
+
+    [Fact]
+    public void Example2()
+    {
+        // Arrange
+        string[] input =
+        [
+            "7 6 4 2 1",
+            "1 2 7 8 9",
+            "9 7 6 2 1",
+            "1 3 2 4 5",
+            "8 6 4 4 1",
+            "1 3 6 7 9"
+        ];
+        Day2Solver solver = new(input);
+
+        // Act
+        long result = solver.SolvePart1();
+
+        // Assert
+        Assert.Equal(2, result);
     }
 }
