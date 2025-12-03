430e22285ebeb14712cc1d814d8064703d841b05
PlopKonijn <plopkonijn+git@gmail.com>
2025-12-03 22:55:58 +0100

    solved part 1

---
+++ AdventOfCode.Solvers.Tests/Year2025/Day1/SecretEntranceSolverTests.cs
@@ -0,0 +1,33 @@
+﻿using AdvenOfCode.Solvers.Year2025.Day1;
+
+namespace AdventOfCode.Solvers.Tests.Year2025.Day1
+{
+    public class SecretEntranceSolverTests
+    {
+        [Fact]
+        public void Example()
+        {
+            //Arrange
+            string[] input = File.ReadAllLines(@"Year2025\Day1\example.txt");
+
+            //Act
+            int result = SecretEntranceSolver.SolvePart1(input);
+
+            //Assert
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void Day1()
+        {
+            //Arrange
+            string[] input = File.ReadAllLines(@"Year2025\Day1\input.txt");
+
+            //Act
+            int result = SecretEntranceSolver.SolvePart1(input);
+
+            //Assert
+            Assert.Equal(995, result);
+        }
+    }
+}
---
+++ src/AdvenOfCode.Solvers/Year2025/Day1/SecretEntranceSolver.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Globalization;
+
+namespace AdvenOfCode.Solvers.Year2025.Day1;
+
+public static class SecretEntranceSolver
+{
+
+
+    public static int SolvePart1(string[] input)
+    {
+        (int _, int result) = input.Select(PareLine)
+                                  .Aggregate(seed: (dialValue: 50, result: 0), Turn);
+
+
+        return result;
+    }
+
+    private static (int dialValue, int result) Turn((int dialValue, int result) t, int distance)
+    {
+        int newValue = (t.dialValue + distance + 100) % 100;
+        return (newValue, t.result + (newValue == 0 ? 1 : 0));
+    }
+
+    private static int PareLine(string line)
+    {
+        int distance = int.Parse(line[1..], CultureInfo.InvariantCulture);
+        return line[0] is 'L' ? -distance : distance;
+    }
+
+
+}
+
+
