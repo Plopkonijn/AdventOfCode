79182ccede85c65d41bf47654c659edcee22ab9e
PlopKonijn <plopkonijn+git@gmail.com>
2025-12-11 00:19:21 +0100

    resolved day7part2 with dynamic programming

--- src/AdvenOfCode.Solvers/Year2025/Day7/LaboratoriesSolver.cs
+++ src/AdvenOfCode.Solvers/Year2025/Day7/LaboratoriesSolver.cs
@@ -35,43 +35,28 @@ public sealed class LaboratoriesSolver(string[] input) : Solver(input)
         return result;
     }
 
-    private sealed record Beam(int Index, long Amount)
-    {
-        public int Index { get; set; } = Index;
-        public long Amount { get; set; } = Amount;
-    }
     public override long SolvePart2()
     {
-        long result = 0;
-        LinkedList<Beam> beams = new([new Beam(Input[0].IndexOf('S', StringComparison.InvariantCulture), 1)]);
-        foreach (string line in Input[1..])
+        int startPosition = Input[0].IndexOf('S', StringComparison.InvariantCulture);
+        long result = SolvePart2(0, startPosition, []);
+        return result;
+    }
+
+    private long SolvePart2(int row, int column, Dictionary<(int row, int column), long> cache)
+    {
+        if (row == Input.Length)
         {
-            for (LinkedListNode<Beam>? n = beams.First; n != null; n = n.Next)
-            {
-                Beam beam = n.Value;
-                if (line[beam.Index] is '^')
-                {
-                    result += beam.Amount;
-                    if (n.Previous is { Value: { } previousBeam } && previousBeam.Index == beam.Index - 1)
-                    {
-                        previousBeam.Amount += beam.Amount;
-                        beam.Index++;
-                    }
-                    else
-                    {
-                        n = beams.AddAfter(n, new Beam(beam.Index + 1, beam.Amount));
-                        beam.Index--;
-                    }
-                }
-                else if (n.Previous is { Value: { } previousBeam } && previousBeam == n.Value)
-                {
-                    previousBeam.Amount += beam.Amount;
-                    LinkedListNode<Beam> t = n.Previous;
-                    beams.Remove(n);
-                    n = t;
-                }
-            }
+            return 1;
         }
-        return beams.Sum(b => b.Amount);
+        if (cache.TryGetValue((row, column), out long result))
+        {
+            return result;
+        }
+
+        result = Input[row][column] is '^'
+            ? SolvePart2(row + 1, column - 1, cache) + SolvePart2(row + 1, column + 1, cache)
+            : SolvePart2(row + 1, column, cache);
+        cache.Add((row, column), result);
+        return result;
     }
 }
