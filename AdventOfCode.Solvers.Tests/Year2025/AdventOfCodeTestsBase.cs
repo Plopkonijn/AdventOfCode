73ae1fa4f4fec83bbf565e8e06e40029db8b2c60
PlopKonijn <plopkonijn+git@gmail.com>
2025-12-20 01:29:44 +0100

    solved day8

--- AdventOfCode.Solvers.Tests/Year2025/AdventOfCodeTestsBase.cs
+++ AdventOfCode.Solvers.Tests/Year2025/AdventOfCodeTestsBase.cs
@@ -2,18 +2,19 @@
 
 namespace AdventOfCode.Solvers.Tests.Year2025
 {
-    public abstract class AdventOfCodeTestsBase<TSolver>
-        where TSolver : Solver
+    public abstract class AdventOfCodeTestsBase
     {
         public abstract string PuzzleInputPath { get; }
-
-        protected abstract TSolver CreateSolver(string[] puzzleInput);
-
         protected string[] ReadPuzzleInput(string inputFileName)
         {
             string path = Path.Combine(PuzzleInputPath, inputFileName);
             return File.ReadAllLines(path);
         }
+    }
+    public abstract class AdventOfCodeTestsBase<TSolver> : AdventOfCodeTestsBase
+        where TSolver : Solver
+    {
+        protected abstract TSolver CreateSolver(string[] puzzleInput);
 
         public abstract void Part1(string inputFilePath, long expectedSolution);
         public abstract void Part2(string inputFilePath, long expectedSolution);
---
+++ src/AdvenOfCode.Solvers/Year2025/Day8/LongVector.cs
@@ -0,0 +1,26 @@
+﻿
+namespace AdvenOfCode.Solvers.Year2025.Day8;
+
+internal sealed record LongVector(long X, long Y, long Z)
+{
+    public long GetLengthSquared()
+    {
+        return (X * X) + (Y * Y) + (Z * Z);
+    }
+
+    public static long DistanceSquared(LongVector a, LongVector b)
+    {
+        return (a - b).GetLengthSquared();
+    }
+
+    internal static LongVector Max(LongVector a, LongVector b)
+    {
+        return new LongVector(long.Max(a.X, b.X), long.Max(a.Y, b.Y), long.Max(a.Z, b.Z));
+    }
+
+    public static LongVector operator -(LongVector a, LongVector b)
+    {
+        return new LongVector(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+    }
+
+}
