efe70f1bd20bff2ac8ae558230205b3204f070d1
PlopKonijn <plopkonijn+git@gmail.com>
2025-12-21 17:47:53 +0100

    solved day 11 part 1

---
+++ AdventOfCode.Solvers.Tests/Year2025/Day11/ReactorSolverTests.cs
@@ -0,0 +1,46 @@
+﻿using AdvenOfCode.Solvers.Year2025.Day11;
+
+namespace AdventOfCode.Solvers.Tests.Year2025.Day11;
+
+public sealed class ReactorSolverTests : AdventOfCodeTestsBase<ReactorSolver>
+{
+    public override string PuzzleInputPath => @"Year2025\Day11";
+
+    [Theory]
+    [InlineData("example.txt", 5)]
+    [InlineData("input.txt", 0)]
+    public override void Part1(string inputFilePath, long expectedSolution)
+    {
+        //Arrange
+        string[] input = ReadPuzzleInput(inputFilePath);
+        ReactorSolver solver = CreateSolver(input);
+
+        //Act
+        long actualSolution = solver.SolvePart1();
+
+        //Assert
+        Assert.Equal(expectedSolution, actualSolution);
+    }
+
+
+    [Theory]
+    [InlineData("example.txt", 0)]
+    [InlineData("input.txt", 0)]
+    public override void Part2(string inputFilePath, long expectedSolution)
+    {
+        //Arrange
+        string[] input = ReadPuzzleInput(inputFilePath);
+        ReactorSolver solver = CreateSolver(input);
+
+        //Act
+        long actualSolution = solver.SolvePart2();
+
+        //Assert
+        Assert.Equal(expectedSolution, actualSolution);
+    }
+
+    protected override ReactorSolver CreateSolver(string[] puzzleInput)
+    {
+        return new ReactorSolver(puzzleInput);
+    }
+}
---
+++ src/AdvenOfCode.Solvers/Year2025/Day11/ReactorSolver.cs
@@ -0,0 +1,35 @@
+﻿namespace AdvenOfCode.Solvers.Year2025.Day11;
+
+public sealed class ReactorSolver(string[] input) : Solver(input)
+{
+
+    public override long SolvePart1()
+    {
+        Dictionary<string, string[]> connections = Input.Select(line => line.Split(':', StringSplitOptions.RemoveEmptyEntries))
+            .ToDictionary(split => split[0], split => split[1].Split(' ', StringSplitOptions.RemoveEmptyEntries));
+        Queue<string> queue = new();
+        queue.Enqueue("you");
+
+        long result = 0;
+        while (queue.TryDequeue(out string? current))
+        {
+            if (current is "out")
+            {
+                result++;
+                continue;
+            }
+            foreach (string neighbour in connections[current])
+            {
+                queue.Enqueue(neighbour);
+            }
+        }
+        return result;
+    }
+
+
+
+    public override long SolvePart2()
+    {
+        throw new NotImplementedException();
+    }
+}
