f5c227028a4572c9be450069c3716eb2db3535e2
PlopKonijn <plopkonijn+git@gmail.com>
2025-12-23 18:31:10 +0100

    solved day 4

---
+++ AdventOfCode.Solvers.Tests/Year2015/Day04/IWasToldThereWouldBeNoMathSolverTests.cs
@@ -0,0 +1,47 @@
+﻿using AdvenOfCode.Solvers;
+using AdvenOfCode.Solvers.Year2015.Day04;
+
+namespace AdventOfCode.Solvers.Tests.Year2015.Day04;
+
+public sealed class IWasToldThereWouldBeNoMathSolverTests : AdventOfCodeTestsBase<TheIdealStockingStufferSolver>
+{
+    public override string PuzzleInputPath => @"Year2015\Day04";
+
+    protected override TheIdealStockingStufferSolver CreateSolver(string[] puzzleInput)
+    {
+        return new TheIdealStockingStufferSolver(puzzleInput);
+    }
+
+    [Theory]
+    [InlineData("example01.txt", 609043)]
+    [InlineData("example02.txt", 1048970)]
+    [InlineData("input.txt", 282749)]
+    public override void Part1(string inputFilePath, long expectedSolution)
+    {
+        //Arrange
+        string[] input = ReadPuzzleInput(inputFilePath);
+        Solver solver = CreateSolver(input);
+
+        //Act
+        long actualSolution = solver.SolvePart1();
+
+        //Assert
+        Assert.Equal(expectedSolution, actualSolution);
+    }
+
+
+    [Theory]
+    [InlineData("input.txt", 9962624)]
+    public override void Part2(string inputFilePath, long expectedSolution)
+    {
+        //Arrange
+        string[] input = ReadPuzzleInput(inputFilePath);
+        Solver solver = CreateSolver(input);
+
+        //Act
+        long actualSolution = solver.SolvePart2();
+
+        //Assert
+        Assert.Equal(expectedSolution, actualSolution);
+    }
+}
---
+++ src/AdvenOfCode.Solvers/Year2015/Day04/TheIdealStockingStufferSolver.cs
@@ -0,0 +1,52 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace AdvenOfCode.Solvers.Year2015.Day04;
+
+#pragma warning disable CA5351 // Do Not Use Broken Cryptographic Algorithms
+public sealed class TheIdealStockingStufferSolver(string[] input) : Solver(input)
+{
+    public override long SolvePart1()
+    {
+        long result = 0;
+        foreach (string line in Input)
+        {
+            return FindHashNumber(line, 5);
+
+        }
+
+        return result;
+    }
+
+    private static long FindHashNumber(string line, int expectedZeros)
+    {
+        long number = 1;
+        while (true)
+        {
+            string secret = $"{line}{number}";
+            byte[] secretBytes = Encoding.ASCII.GetBytes(secret);
+            byte[] hashBytes = MD5.HashData(secretBytes);
+            string hash = BitConverter.ToString(hashBytes);
+            int count = hash.Where(char.IsAsciiLetterOrDigit).ToArray()
+                .TakeWhile(c => c == '0')
+                .Count();
+            if (count == expectedZeros)
+            {
+                return number;
+            }
+            number++;
+        }
+    }
+
+    public override long SolvePart2()
+    {
+        long result = 0;
+        foreach (string line in Input)
+        {
+            return FindHashNumber(line, 6);
+
+        }
+
+        return result;
+    }
+}
