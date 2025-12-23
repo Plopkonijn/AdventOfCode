8ffea983a2c1f1e7e0e832be06d0d012a66b8f7f
PlopKonijn <plopkonijn+git@gmail.com>
2025-12-23 22:35:49 +0100

    solved day 6 part 1

---
+++ src/AdvenOfCode.Solvers/Year2015/Day06/LightGrid.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+
+namespace AdvenOfCode.Solvers.Year2015.Day06;
+
+internal sealed class LightGrid
+{
+    private readonly BitArray _lights = new(1000 * 1000);
+
+    internal long LitLightCount()
+    {
+        return _lights.OfType<bool>().Count(b => b);
+    }
+
+    internal void Toggle(IntLocation start, IntLocation end)
+    {
+        PerformInstruction(start, end, b => !b);
+    }
+
+    internal void TurnOff(IntLocation start, IntLocation end)
+    {
+        PerformInstruction(start, end, b => false);
+    }
+
+    internal void TurnOn(IntLocation start, IntLocation end)
+    {
+        PerformInstruction(start, end, b => true);
+    }
+
+    internal void PerformInstruction(IntLocation start, IntLocation end, Func<bool, bool> instruction)
+    {
+        for (int x = start.X; x <= end.X; x++)
+        {
+            for (int y = start.Y; y <= end.Y; y++)
+            {
+                _lights[x + (1000 * y)] = instruction(_lights[x + (1000 * y)]);
+            }
+        }
+    }
+}
