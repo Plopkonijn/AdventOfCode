2fb45e9deb90f44c8c29cb3ccf7f195b59a5afa5
PlopKonijn <plopkonijn+git@gmail.com>
2025-12-23 17:30:59 +0100

    solved day 2

---
+++ src/AdvenOfCode.Solvers/Year2015/Day02/Box.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace AdvenOfCode.Solvers.Year2015.Day02;
+
+internal sealed record Box(long Length, long Width, long Height)
+{
+    public static Box Parse(string input)
+    {
+        string[] split = input.Split('x');
+        long length = long.Parse(split[0], CultureInfo.InvariantCulture);
+        long width = long.Parse(split[1], CultureInfo.InvariantCulture);
+        long height = long.Parse(split[2], CultureInfo.InvariantCulture);
+        return new Box(length, width, height);
+    }
+}
---
+++ src/AdvenOfCode.Solvers/Year2015/Day02/IWasToldThereWouldBeNoMathSolver.cs
@@ -0,0 +1,62 @@
+﻿namespace AdvenOfCode.Solvers.Year2015.Day02;
+
+public sealed class IWasToldThereWouldBeNoMathSolver(string[] input) : Solver(input)
+{
+    public override long SolvePart1()
+    {
+        long result = 0;
+        foreach (string line in Input)
+        {
+            Box box = Box.Parse(line);
+            result += WrappingPaperArea(box);
+        }
+        return result;
+    }
+
+    private static long WrappingPaperArea(Box box)
+    {
+        return TotalArea(box) + SmallesSideArea(box);
+    }
+
+    private static long TotalArea(Box box)
+    {
+        return 2 * ((box.Length * box.Width) + (box.Width * box.Height) + (box.Height * box.Length));
+    }
+
+    private static long SmallesSideArea(Box box)
+    {
+        long top = box.Length * box.Width;
+        long front = box.Width * box.Height;
+        long side = box.Height * box.Length;
+        return Math.Min(Math.Min(top, front), side);
+    }
+
+    public override long SolvePart2()
+    {
+        long result = 0;
+        foreach (string line in Input)
+        {
+            Box box = Box.Parse(line);
+            result += TotalRibbonLength(box);
+        }
+        return result;
+    }
+
+    private static long TotalRibbonLength(Box box)
+    {
+        return SmallesRibbonLength(box) + BowLength(box);
+    }
+
+    private static long SmallesRibbonLength(Box box)
+    {
+        long top = box.Length + box.Width;
+        long front = box.Width + box.Height;
+        long side = box.Height + box.Length;
+        return 2 * Math.Min(Math.Min(top, front), side);
+    }
+
+    private static long BowLength(Box box)
+    {
+        return box.Length * box.Width * box.Height;
+    }
+}
