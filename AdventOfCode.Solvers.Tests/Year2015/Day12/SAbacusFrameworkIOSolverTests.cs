5f4754c73c404e5fa839ad7bacee7392df33ef8f
PlopKonijn <plopkonijn+git@gmail.com>
2025-12-25 16:37:07 +0100

    solved day 12 part 1

---
+++ AdventOfCode.Solvers.Tests/Year2015/Day12/SAbacusFrameworkIOSolverTests.cs
@@ -0,0 +1,45 @@
+﻿using AdvenOfCode.Solvers;
+using AdvenOfCode.Solvers.Year2015.Day12;
+
+namespace AdventOfCode.Solvers.Tests.Year2015.Day12;
+
+public sealed class SAbacusFrameworkIOSolverTests : AdventOfCodeTestsBase<SAbacusFrameworkIOSolver>
+{
+    public override string PuzzleInputPath => @"Year2015\Day12";
+
+    protected override SAbacusFrameworkIOSolver CreateSolver(string[] puzzleInput)
+    {
+        return new SAbacusFrameworkIOSolver(puzzleInput);
+    }
+
+    [Theory]
+    [InlineData("input.txt", 156366)]
+    public override void Part1(string inputFilePath, long expectedSolution)
+    {
+        //Arrange
+        string[] input = ReadPuzzleInput(inputFilePath);
+        Solver solver = CreateSolver(input);
+
+        //Act
+        long actualSolution = solver.SolvePart1();
+
+        //Assert
+        Assert.Equal(expectedSolution, actualSolution);
+    }
+
+
+    [Theory]
+    [InlineData("input.txt", 0)]
+    public override void Part2(string inputFilePath, long expectedSolution)
+    {
+        //Arrange
+        string[] input = ReadPuzzleInput(inputFilePath);
+        Solver solver = CreateSolver(input);
+
+        //Act
+        long actualSolution = solver.SolvePart2();
+
+        //Assert
+        Assert.Equal(expectedSolution, actualSolution);
+    }
+}
---
+++ src/AdvenOfCode.Solvers/Year2015/Day12/SAbacusFrameworkIOSolver.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+namespace AdvenOfCode.Solvers.Year2015.Day12;
+
+public sealed class SAbacusFrameworkIOSolver(string[] input) : Solver(input)
+{
+    public override long SolvePart1()
+    {
+        long result = 0;
+        foreach (string line in Input)
+        {
+            result += SumNumbers(line);
+        }
+        return result;
+    }
+
+    private static long SumNumbers(ReadOnlySpan<char> line)
+    {
+        long result = 0;
+        while (line.Length > 0)
+        {
+            if (line[0] != '-' && !char.IsDigit(line[0]))
+            {
+                line = line[1..];
+                continue;
+            }
+            int nonDigitIndex = 1;
+            while (line.Length > 0 && char.IsDigit(line[nonDigitIndex]))
+            {
+                nonDigitIndex++;
+            }
+            ReadOnlySpan<char> digit = line[0..nonDigitIndex];
+            result += long.Parse(digit, CultureInfo.InvariantCulture);
+            line = line[nonDigitIndex..];
+        }
+        return result;
+    }
+
+    public override long SolvePart2()
+    {
+        throw new NotImplementedException();
+    }
+}
