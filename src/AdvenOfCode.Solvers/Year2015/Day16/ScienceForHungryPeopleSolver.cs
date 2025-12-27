59900587c174eca562af8ded53b764012dc839a2
PlopKonijn <plopkonijn+git@gmail.com>
2025-12-28 00:57:48 +0100

    solved day 16 part 1

---
+++ src/AdvenOfCode.Solvers/Year2015/Day16/ScienceForHungryPeopleSolver.cs
@@ -0,0 +1,29 @@
+﻿namespace AdvenOfCode.Solvers.Year2015.Day16;
+
+public sealed class ScienceForHungryPeopleSolver(string[] Input) : Solver(Input)
+{
+    public override long SolvePart1()
+    {
+        HashSet<string> properties = [
+            "children: 3",
+            "cats: 7",
+            "samoyeds: 2",
+            "pomeranians: 3",
+            "akitas: 0",
+            "vizslas: 0",
+            "goldfish: 5",
+            "trees: 3",
+            "cars: 2",
+            "perfumes: 1",
+            ];
+        List<Sue> sues = Input.Select(Sue.Parse)
+                        .ToList();
+        Sue sue = sues.First(s => properties.IsProperSupersetOf(s.Properties));
+        return sue.Number;
+    }
+
+    public override long SolvePart2()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/AdvenOfCode.Solvers/Year2015/Day16/Sue.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace AdvenOfCode.Solvers.Year2015.Day16;
+
+internal sealed record Sue(int Number, List<string> Properties)
+{
+    internal static Sue Parse(string line)
+    {
+        Match match = Regex.Match(line, @"Sue (?<sue>\d+):( (?<property>\w+: \d+),?)+");
+        if (!match.Success)
+        {
+            throw new InvalidOperationException();
+        }
+
+        int number = int.Parse(match.Groups["sue"].Value, CultureInfo.InvariantCulture);
+        List<string> properties = match.Groups["property"].Captures.Select(c => c.Value).ToList();
+        return new Sue(number, properties);
+    }
+}
