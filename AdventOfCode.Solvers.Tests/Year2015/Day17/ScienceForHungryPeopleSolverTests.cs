609da9948aff55f7cea505888d63b47c851a919c
PlopKonijn <plopkonijn+git@gmail.com>
2025-12-28 01:35:27 +0100

    solved day 17 part 1

---
+++ AdventOfCode.Solvers.Tests/Year2015/Day17/ScienceForHungryPeopleSolverTests.cs
@@ -0,0 +1,46 @@
+﻿using AdvenOfCode.Solvers.Year2015.Day17;
+
+namespace AdventOfCode.Solvers.Tests.Year2015.Day17;
+
+public sealed class ScienceForHungryPeopleSolverTests : AdventOfCodeTestsBase
+{
+    public override string PuzzleInputPath => @"Year2015\Day17";
+
+    [Theory]
+    [InlineData("example01.txt", 25, 4)]
+    [InlineData("input.txt", 150, 0)]
+    public void Part1(string inputFilePath, long totalVolume, long expectedSolution)
+    {
+        //Arrange
+        string[] input = ReadPuzzleInput(inputFilePath);
+        ScienceForHungryPeopleSolver solver = CreateSolver(input);
+
+        //Act
+        long actualSolution = solver.SolvePart1(totalVolume);
+
+        //Assert
+        Assert.Equal(expectedSolution, actualSolution);
+    }
+
+
+    [Theory]
+    [InlineData("example01.txt", 0)]
+    [InlineData("input.txt", 0)]
+    public void Part2(string inputFilePath, long expectedSolution)
+    {
+        //Arrange
+        string[] input = ReadPuzzleInput(inputFilePath);
+        ScienceForHungryPeopleSolver solver = CreateSolver(input);
+
+        //Act
+        long actualSolution = solver.SolvePart2();
+
+        //Assert
+        Assert.Equal(expectedSolution, actualSolution);
+    }
+
+    private ScienceForHungryPeopleSolver CreateSolver(string[] puzzleInput)
+    {
+        return new ScienceForHungryPeopleSolver(puzzleInput);
+    }
+}
---
+++ src/AdvenOfCode.Solvers/Year2015/Day17/ScienceForHungryPeopleSolver.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+
+namespace AdvenOfCode.Solvers.Year2015.Day17;
+
+public sealed class ScienceForHungryPeopleSolver(string[] Input)
+{
+    public long SolvePart1(long totalVolume)
+    {
+        long[] containerVolumes = Input.Select(line => long.Parse(line, CultureInfo.InvariantCulture))
+            .ToArray();
+        return Calculate(totalVolume, containerVolumes, []);
+    }
+
+    private static long Calculate(long totalVolume, ReadOnlySpan<long> containerVolume, Dictionary<(long, int), long> cache)
+    {
+        if (cache.TryGetValue((totalVolume, containerVolume.Length), out long result))
+        {
+            return result;
+        }
+        if (containerVolume.Length == 0)
+        {
+            result = totalVolume == 0 ? 1 : 0;
+        }
+        else if (totalVolume < 0)
+        {
+            result = 0;
+        }
+        else
+        {
+            result = totalVolume == 0
+                ? 1
+                : Calculate(totalVolume, containerVolume[1..], cache) + Calculate(totalVolume - containerVolume[0], containerVolume[1..], cache);
+        }
+        cache.Add((totalVolume, containerVolume.Length), result);
+        return result;
+    }
+
+    public long SolvePart2()
+    {
+        throw new NotImplementedException();
+    }
+}
