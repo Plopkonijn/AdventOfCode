00a9354f9d5716168a5cadb2eebf9e31c7af6ce7
PlopKonijn <plopkonijn+git@gmail.com>
2025-12-29 00:59:05 +0100

    solved day 18 part 1

---
+++ AdventOfCode.Solvers.Tests/Year2015/Day18/LikeAGifForYourYardSolverTests.cs
@@ -0,0 +1,46 @@
+﻿using AdvenOfCode.Solvers.Year2015.Day18;
+
+namespace AdventOfCode.Solvers.Tests.Year2015.Day18;
+
+public sealed class LikeAGifForYourYardSolverTests : AdventOfCodeTestsBase
+{
+    public override string PuzzleInputPath => @"Year2015\Day18";
+
+    [Theory]
+    [InlineData("example01.txt", 4, 4)]
+    [InlineData("input.txt", 100, 0)]
+    public void Part1(string inputFilePath, int steps, long expectedSolution)
+    {
+        //Arrange
+        string[] input = ReadPuzzleInput(inputFilePath);
+        LikeAGifForYourYardSolver solver = CreateSolver(input);
+
+        //Act
+        long actualSolution = solver.SolvePart1(steps);
+
+        //Assert
+        Assert.Equal(expectedSolution, actualSolution);
+    }
+
+
+    [Theory]
+    [InlineData("example01.txt", 0)]
+    [InlineData("input.txt", 0)]
+    public void Part2(string inputFilePath, long expectedSolution)
+    {
+        //Arrange
+        string[] input = ReadPuzzleInput(inputFilePath);
+        LikeAGifForYourYardSolver solver = CreateSolver(input);
+
+        //Act
+        long actualSolution = solver.SolvePart2();
+
+        //Assert
+        Assert.Equal(expectedSolution, actualSolution);
+    }
+
+    private LikeAGifForYourYardSolver CreateSolver(string[] puzzleInput)
+    {
+        return new LikeAGifForYourYardSolver(puzzleInput);
+    }
+}
---
+++ src/AdvenOfCode.Solvers/Year2015/Day18/LikeAGifForYourYardSolver.cs
@@ -0,0 +1,73 @@
+﻿namespace AdvenOfCode.Solvers.Year2015.Day18;
+
+public sealed class LikeAGifForYourYardSolver(string[] Input)
+{
+    public long SolvePart1(int steps)
+    {
+        LightGrid gridCurrent = ParseInput();
+        LightGrid gridNext = new(gridCurrent.Width, gridCurrent.Height);
+        for (int step = 0; step < steps; step++)
+        {
+            for (int x = 0; x < gridCurrent.Width; x++)
+            {
+                for (int y = 0; y < gridCurrent.Height; y++)
+                {
+                    var turnedOnNeighbours = gridCurrent.TurnedOnNeighbours(x, y);
+                    if (gridCurrent.IsOn(x, y))
+                    {
+                        if (turnedOnNeighbours is 2 or 3)
+                        {
+                            gridNext.TurnOn(x, y);
+                        }
+                        else
+                        {
+                            gridNext.TurnOff(x, y);
+                        }
+                    }
+                    else
+                    {
+                        if (turnedOnNeighbours is 3)
+                        {
+                            gridNext.TurnOn(x, y);
+                        }
+                        else
+                        {
+                            gridNext.TurnOff(x, y);
+                        }
+                    }
+                }
+            }
+            (gridCurrent, gridNext) = (gridNext, gridCurrent);
+        }
+
+        return gridCurrent.LitLightCount();
+    }
+
+    private LightGrid ParseInput()
+    {
+        LightGrid grid = new(Input[0].Length, Input.Length);
+        for (int y = 0; y < Input.Length; y++)
+        {
+            string line = Input[y];
+            for (int x = 0; x < line.Length; x++)
+            {
+                char light = line[x];
+                switch (light)
+                {
+                    case '#':
+                        grid.TurnOn(x, y);
+                        break;
+                    case '.':
+                        grid.TurnOff(x, y);
+                        break;
+                }
+            }
+        }
+        return grid;
+    }
+
+    public long SolvePart2()
+    {
+        throw new NotImplementedException();
+    }
+}
