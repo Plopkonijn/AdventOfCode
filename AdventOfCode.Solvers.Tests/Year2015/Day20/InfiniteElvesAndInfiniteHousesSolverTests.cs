9d3bda2b77e7d06c7c1eb666a5bb77cd381cd06f
PlopKonijn <plopkonijn+git@gmail.com>
2025-12-29 16:15:04 +0100

    solved day 20

---
+++ AdventOfCode.Solvers.Tests/Year2015/Day20/InfiniteElvesAndInfiniteHousesSolverTests.cs
@@ -0,0 +1,45 @@
+﻿using AdvenOfCode.Solvers;
+using AdvenOfCode.Solvers.Year2015.Day20;
+
+namespace AdventOfCode.Solvers.Tests.Year2015.Day20;
+
+public sealed class InfiniteElvesAndInfiniteHousesSolverTests : AdventOfCodeTestsBase<InfiniteElvesAndInfiniteHousesSolver>
+{
+    public override string PuzzleInputPath => @"Year2015\Day20";
+
+    protected override InfiniteElvesAndInfiniteHousesSolver CreateSolver(string[] puzzleInput)
+    {
+        return new InfiniteElvesAndInfiniteHousesSolver(puzzleInput);
+    }
+
+    [Theory]
+    [InlineData("input.txt", 831600)]
+    public override void Part1(string inputFilePath, long expectedSolution)
+    {
+        //Arrange
+        string[] input = ReadPuzzleInput(inputFilePath);
+        Solver solver = CreateSolver(input);
+
+        //Act
+        long actualSolution = solver.SolvePart1();
+
+        //Assert
+        Assert.Equal(expectedSolution, actualSolution);
+    }
+
+
+    [Theory]
+    [InlineData("input.txt", 884520)]
+    public override void Part2(string inputFilePath, long expectedSolution)
+    {
+        //Arrange
+        string[] input = ReadPuzzleInput(inputFilePath);
+        Solver solver = CreateSolver(input);
+
+        //Act
+        long actualSolution = solver.SolvePart2();
+
+        //Assert
+        Assert.Equal(expectedSolution, actualSolution);
+    }
+}
---
+++ src/AdvenOfCode.Solvers/Year2015/Day20/InfiniteElvesAndInfiniteHousesSolver.cs
@@ -0,0 +1,63 @@
+﻿using System.Globalization;
+
+namespace AdvenOfCode.Solvers.Year2015.Day20;
+
+public sealed class InfiniteElvesAndInfiniteHousesSolver(string[] input) : Solver(input)
+{
+    public override long SolvePart1()
+    {
+        long targetPresentCount = long.Parse(Input[0], CultureInfo.InvariantCulture);
+        for (long house = 1; ; house++)
+        {
+            long presentCount = 0;
+            for (long d = 1; d <= (long)Math.Sqrt(house); d++)
+            {
+                if (house % d != 0)
+                {
+                    continue;
+                }
+                presentCount += d;
+                long dd = house / d;
+                if (dd != d)
+                {
+                    presentCount += dd;
+                }
+            }
+            presentCount *= 10;
+            if (presentCount >= targetPresentCount)
+            {
+                return house;
+            }
+        }
+    }
+
+    public override long SolvePart2()
+    {
+        long targetPresentCount = long.Parse(Input[0], CultureInfo.InvariantCulture);
+        for (long house = 1; ; house++)
+        {
+            long presentCount = 0;
+            for (long d = 1; d <= (long)Math.Sqrt(house); d++)
+            {
+                if (house % d != 0)
+                {
+                    continue;
+                }
+                long dd = house / d;
+                if (dd < 50)
+                {
+                    presentCount += d;
+                }
+                if (dd != d && d < 50)
+                {
+                    presentCount += dd;
+                }
+            }
+            presentCount *= 11;
+            if (presentCount >= targetPresentCount)
+            {
+                return house;
+            }
+        }
+    }
+}
