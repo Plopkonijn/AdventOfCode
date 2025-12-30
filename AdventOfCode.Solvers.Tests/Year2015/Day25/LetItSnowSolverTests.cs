ca7d21c38de13cea019c143d86f0db44761825a5
PlopKonijn <plopkonijn+git@gmail.com>
2025-12-31 00:32:24 +0100

    solved day 25 part 1

---
+++ AdventOfCode.Solvers.Tests/Year2015/Day25/LetItSnowSolverTests.cs
@@ -0,0 +1,45 @@
+﻿using AdvenOfCode.Solvers;
+using AdvenOfCode.Solvers.Year2015.Day25;
+
+namespace AdventOfCode.Solvers.Tests.Year2015.Day25;
+
+public sealed class LetItSnowSolverTests : AdventOfCodeTestsBase<LetItSnowSolver>
+{
+    public override string PuzzleInputPath => @"Year2015\Day25";
+
+    protected override LetItSnowSolver CreateSolver(string[] puzzleInput)
+    {
+        return new LetItSnowSolver(puzzleInput);
+    }
+
+    [Theory]
+    [InlineData("input.txt", 0)]
+    public override void Part1(string inputFilePath, long expectedSolution)
+    {
+        //Arrange
+        string[] input = ReadPuzzleInput(inputFilePath);
+        Solver solver = CreateSolver(input);
+
+        //Act
+        long actualSolution = solver.SolvePart1();
+
+        //Assert
+        Assert.Equal(expectedSolution, actualSolution);
+    }
+
+
+    [Theory]
+    [InlineData("input.txt", 0)]
+    public override void Part2(string inputFilePath, long expectedSolution)
+    {
+        //Arrange
+        string[] input = ReadPuzzleInput(inputFilePath);
+        Solver solver = CreateSolver(input);
+
+        //Act
+        long actualSolution = solver.SolvePart2();
+
+        //Assert
+        Assert.Equal(expectedSolution, actualSolution);
+    }
+}
