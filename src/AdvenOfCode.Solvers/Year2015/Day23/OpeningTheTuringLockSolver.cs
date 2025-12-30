e262f31fedfc9559d3e3be51c97ac56e485587e0
PlopKonijn <plopkonijn+git@gmail.com>
2025-12-30 13:27:12 +0100

    solved day 23 part 1

---
+++ src/AdvenOfCode.Solvers/Year2015/Day23/OpeningTheTuringLockSolver.cs
@@ -0,0 +1,112 @@
+﻿using System.Globalization;
+
+namespace AdvenOfCode.Solvers.Year2015.Day23;
+
+public sealed partial class OpeningTheTuringLockSolver(string[] input) : Solver(input)
+{
+    public override long SolvePart1()
+    {
+        Register registerA = new("a");
+        Register registerB = new("b");
+        int instructionIndex = 0;
+        while (instructionIndex < Input.Length)
+        {
+            ReadOnlySpan<char> instruction = Input[instructionIndex];
+            Register register;
+            ReadOnlySpan<char> jump;
+            ReadOnlySpan<char> operation = instruction[0..3];
+            switch (operation)
+            {
+                case "hlf":
+                    register = PickRegister(registerA, registerB, instruction);
+                    register.Half();
+                    instructionIndex++;
+                    break;
+                case "tpl":
+                    register = PickRegister(registerA, registerB, instruction);
+                    register.Triple();
+                    instructionIndex++;
+                    break;
+                case "inc":
+                    register = PickRegister(registerA, registerB, instruction);
+                    register.Increment();
+                    instructionIndex++;
+                    break;
+                case "jmp":
+                    jump = instruction[4..];
+                    instructionIndex += int.Parse(jump, CultureInfo.InvariantCulture);
+                    break;
+                case "jie":
+                    register = PickRegister(registerA, registerB, instruction);
+                    if (register.IsEven())
+                    {
+                        jump = instruction[7..];
+                        instructionIndex += int.Parse(jump, CultureInfo.InvariantCulture);
+                    }
+                    else
+                    {
+                        instructionIndex++;
+                    }
+                    break;
+                case "jio":
+                    register = PickRegister(registerA, registerB, instruction);
+                    if (register.IsOne())
+                    {
+                        jump = instruction[7..];
+                        instructionIndex += int.Parse(jump, CultureInfo.InvariantCulture);
+                    }
+                    else
+                    {
+                        instructionIndex++;
+                    }
+                    break;
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+        return (long)registerB.Value;
+    }
+
+    private static Register PickRegister(Register registerA, Register registerB, ReadOnlySpan<char> instruction)
+    {
+        return instruction[4] switch
+        {
+            'a' => registerA,
+            'b' => registerB,
+            _ => throw new InvalidOperationException()
+        };
+    }
+
+    public override long SolvePart2()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+internal sealed record Register(string Name)
+{
+    public ulong Value { get; private set; }
+    public void Half()
+    {
+        Value /= 2;
+    }
+    public void Triple()
+    {
+        Value *= 3;
+    }
+
+    public void Increment()
+    {
+        Value++;
+    }
+
+    public bool IsEven()
+    {
+        return Value % 2 == 0;
+    }
+
+    public bool IsOne()
+    {
+        return Value == 1;
+    }
+}
