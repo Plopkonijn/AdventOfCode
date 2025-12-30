32a782cf5d777756dd9468dc12690310145ac708
PlopKonijn <plopkonijn+git@gmail.com>
2025-12-31 00:03:20 +0100

    attempt at day 24

---
+++ AdventOfCode.Solvers.Tests/Year2015/Day24/ItHangsInTheBalanceSolverTests.cs
@@ -0,0 +1,46 @@
+﻿using AdvenOfCode.Solvers;
+using AdvenOfCode.Solvers.Year2015.Day24;
+
+namespace AdventOfCode.Solvers.Tests.Year2015.Day24;
+
+public sealed class ItHangsInTheBalanceSolverTests : AdventOfCodeTestsBase<ItHangsInTheBalanceSolver>
+{
+    public override string PuzzleInputPath => @"Year2015\Day24";
+
+    protected override ItHangsInTheBalanceSolver CreateSolver(string[] puzzleInput)
+    {
+        return new ItHangsInTheBalanceSolver(puzzleInput);
+    }
+
+    [Theory]
+    [InlineData("example01.txt", 99)]
+    [InlineData("input.txt", 0)]
+    public override void Part1(string inputFilePath, long expectedSolution)
+    {
+        //Arrange
+        string[] input = ReadPuzzleInput(inputFilePath);
+        Solver solver = CreateSolver(input);
+
+        //Act
+        long actualSolution = solver.SolvePart1();
+
+        //Assert
+        Assert.Equal(expectedSolution, actualSolution);
+    }
+
+
+    [Theory]
+    [InlineData("input.txt", 0)]
+    public override void Part2(string inputFilePath, long expectedSolution)
+    {
+        //Arrange
+        string[] input = ReadPuzzleInput(inputFilePath);
+        Solver solver = CreateSolver(input);
+
+        //Act
+        long actualSolution = solver.SolvePart2();
+
+        //Assert
+        Assert.Equal(expectedSolution, actualSolution);
+    }
+}
---
+++ src/AdvenOfCode.Solvers/Year2015/Day24/ItHangsInTheBalanceSolver.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Specialized;
+using System.Globalization;
+
+namespace AdvenOfCode.Solvers.Year2015.Day24;
+
+public sealed partial class ItHangsInTheBalanceSolver(string[] input) : Solver(input)
+{
+    public override long SolvePart1()
+    {
+        long[] weights = Input.Select(line => long.Parse(line, CultureInfo.InvariantCulture))
+            .OrderDescending()
+            .ToArray();
+        long totalWeight = weights.Sum();
+        if (totalWeight % 3 != 0)
+        {
+            throw new InvalidOperationException();
+        }
+        long groupWeight = totalWeight / 3;
+        return GetMinimumEntanglement(groupWeight, new Distribution(), weights, []) ?? throw new InvalidOperationException();
+    }
+
+    private static long? GetMinimumEntanglement(long maxWeight, Distribution distribution, ReadOnlySpan<long> weights, Dictionary<Distribution, long?> cache)
+    {
+        if (cache.TryGetValue(distribution, out long? result))
+        {
+            return result;
+        }
+        if (weights.Length == 0)
+        {
+            if (distribution.IsValid())
+            {
+                return 1;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        if (distribution.FirstWeight > maxWeight || distribution.SecondWeight > maxWeight || distribution.ThirdWeight > maxWeight)
+        {
+            return null;
+        }
+
+        int mask = 1 << (weights.Length - 1);
+        result = null;
+        Distribution first = distribution with
+        {
+            FirstWeight = distribution.FirstWeight + weights[0],
+            FirstIndices = new BitVector32(distribution.FirstIndices.Data | mask)
+        };
+        if (GetMinimumEntanglement(maxWeight, first, weights[1..], cache) is { } firstEntanglement && firstEntanglement * weights[0] < result)
+        {
+            result = firstEntanglement * weights[0];
+        }
+        Distribution second = distribution with
+        {
+            SecondWeight = distribution.SecondWeight + weights[0],
+            SecondIndices = new BitVector32(distribution.SecondIndices.Data | mask)
+        };
+        if (GetMinimumEntanglement(maxWeight, second, weights[1..], cache) is { } secondEntanglement && secondEntanglement < result)
+        {
+            result = secondEntanglement;
+        }
+        Distribution third = distribution with
+        {
+            ThirdWeight = distribution.ThirdWeight + weights[0],
+            ThirdIndices = new BitVector32(distribution.ThirdIndices.Data | mask)
+        };
+        if (GetMinimumEntanglement(maxWeight, third, weights[1..], cache) is { } thirdEntanglement && thirdEntanglement < result)
+        {
+            result = thirdEntanglement;
+        }
+        cache.Add(distribution, result);
+        return result;
+    }
+
+    public override long SolvePart2()
+    {
+        throw new NotImplementedException();
+    }
+}
