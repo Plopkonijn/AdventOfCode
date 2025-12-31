6be1260c623581132c7c13bafc3ab66dfbf2367b
PlopKonijn <plopkonijn+git@gmail.com>
2025-12-31 01:33:03 +0100

    ugh

--- src/AdvenOfCode.Solvers/Year2015/Day22/WizardSimulator20XXSolver.cs
+++ src/AdvenOfCode.Solvers/Year2015/Day22/WizardSimulator20XXSolver.cs
@@ -5,56 +5,88 @@ namespace AdvenOfCode.Solvers.Year2015.Day22;
 
 public sealed partial class WizardSimulator20XXSolver(string[] input) : Solver(input)
 {
-    private readonly List<Spell> _spells =
+    private readonly Spell[] _spells =
     [
-        new MagicMissile(),
-        new Drain(),
-        new Shield(),
-        new Poison(),
-        new Recharge(),
+        new Spell("Magic Missile",53,Damage:4),
+        new Spell("Drain",73,Damage:2,Healing:2),
+        new Spell("Shield",113,Armor:7,Turns:6),
+        new Spell("Poison", 173,Damage:3,Turns:6),
+        new Spell("Recharge",229,Recharge:4,Turns:5)
     ];
 
     public override long SolvePart1()
     {
-        Player player = new(50, 0, 500);
-        Boss boss = ParseBoss();
-        GameState turn = new(0, player, boss, []);
-        Stack<GameState> stack = new();
-        stack.Push(turn);
-        long leastMana = long.MaxValue;
-        while (stack.TryPop(out GameState? current))
-        {
-            if (current.TotalSpendMana > leastMana)
-            {
-                continue;
-            }
-            if (current.IsFinished)
-            {
-                if (current.PlayerHasWon && current.TotalSpendMana < leastMana)
-                {
-                    leastMana = current.TotalSpendMana;
-                }
-                continue;
-            }
-            foreach (Spell spell in _spells)
-            {
-                if (spell.Cost > current.Player.Mana ||
-                    current.Effects.Any(e => e.GetType() == spell.GetType()))
-                {
-                    continue;
-                }
-                GameState next = current.CastSpell(spell with { });
-                stack.Push(next);
-            }
-        }
-        return leastMana;
+        long playerHP = 50;
+        long playerMana = 500;
+        long bossHP = long.Parse(DigitRegex().Match(Input[0]).Value, CultureInfo.InvariantCulture);
+        long bossDamage = long.Parse(DigitRegex().Match(Input[1]).Value, CultureInfo.InvariantCulture);
+
+        return Calculate(playerHP, playerMana, bossHP, bossDamage, []) ?? throw new InvalidOperationException();
     }
 
-    private Boss ParseBoss()
+    private long? Calculate(long playerHP, long playerMana, long bossHP, long bossDamage, List<Spell> effects)
     {
-        long hitPoints = long.Parse(DigitRegex().Match(Input[0]).Value, CultureInfo.InvariantCulture);
-        long damage = long.Parse(DigitRegex().Match(Input[1]).Value, CultureInfo.InvariantCulture);
-        return new Boss(hitPoints, damage);
+        // Activate effects before players turn
+        bossHP -= effects.Sum(e => e.Damage);
+        if (bossHP <= 0)
+        {
+            return 0;
+        }
+        playerHP += effects.Sum(e => e.Healing);
+        playerMana += effects.Sum(e => e.Recharge);
+        effects.ForEach(e => e.Turns--);
+        _ = effects.RemoveAll(e => e.Turns == 0);
+
+        long? leastMana = null;
+        foreach (Spell spell in _spells)
+        {
+            // players turn
+            if (effects.Any(e => e.Name == spell.Name))
+            {
+                continue;
+            }
+            else if (spell.Cost > playerMana)
+            {
+                continue;
+            }
+            long newPlayerMana = playerMana - spell.Cost;
+            List<Spell> newEffects = effects.Select(e => e with { }).ToList();
+            newEffects.Add(spell);
+
+
+            // Activate effects before boss turn
+            long newBossHP = bossHP - effects.Sum(e => e.Damage);
+            if (newBossHP <= 0)
+            {
+                if (spell.Cost < leastMana)
+                {
+                    leastMana = spell.Cost;
+                    continue;
+                }
+            }
+            long newPlayerHP = playerHP + effects.Sum(e => e.Healing);
+            newPlayerMana += effects.Sum(e => e.Recharge);
+            newEffects.ForEach(e => e.Turns--);
+            _ = newEffects.RemoveAll(e => e.Turns == 0);
+
+            //boss turn
+            newPlayerHP -= Math.Max(1, bossDamage - effects.Sum(e => e.Armor));
+            if (newPlayerHP <= 0)
+            {
+                continue;
+            }
+            if (Calculate(newPlayerHP, newPlayerMana, newBossHP, bossDamage, newEffects) is not { } mana)
+            {
+                continue;
+            }
+            mana += spell.Cost;
+            if (!leastMana.HasValue || mana < leastMana.Value)
+            {
+                leastMana = mana;
+            }
+        }
+
+        return leastMana;
     }
 
     public override long SolvePart2()
@@ -66,3 +98,7 @@ public sealed partial class WizardSimulator20XXSolver(string[] input) : Solver(i
     private static partial Regex DigitRegex();
 }
 
+internal sealed record Spell(string Name, long Cost, long Damage = 0, long Healing = 0, long Armor = 0, long Recharge = 0, long Turns = 1)
+{
+    public long Turns { get; set; } = Turns;
+}
