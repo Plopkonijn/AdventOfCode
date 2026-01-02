b595e5765df373470b144f160bb55f27f5626482
PlopKonijn <plopkonijn+git@gmail.com>
2026-01-02 15:45:47 +0100

    finishing up the year

--- AdventOfCode.Solvers.Tests/Year2015/Day25/LetItSnowSolverTests.cs
+++ AdventOfCode.Solvers.Tests/Year2015/Day25/LetItSnowSolverTests.cs
@@ -28,18 +28,8 @@ public sealed class LetItSnowSolverTests : AdventOfCodeTestsBase<LetItSnowSolver
     }
 
 
-    [Theory]
-    [InlineData("input.txt", 0)]
     public override void Part2(string inputFilePath, long expectedSolution)
     {
-        //Arrange
-        string[] input = ReadPuzzleInput(inputFilePath);
-        Solver solver = CreateSolver(input);
-
-        //Act
-        long actualSolution = solver.SolvePart2();
-
-        //Assert
-        Assert.Equal(expectedSolution, actualSolution);
+        throw new NotImplementedException("There was no part 2");
     }
 }
