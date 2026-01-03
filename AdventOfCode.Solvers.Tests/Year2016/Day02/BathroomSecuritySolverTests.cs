950a2c6a564c25ea1f836259548f6706a85c340f
PlopKonijn <plopkonijn+git@gmail.com>
2026-01-03 23:08:08 +0100

    solved day 2

---
+++ AdventOfCode.Solvers.Tests/Year2016/Day02/BathroomSecuritySolverTests.cs
@@ -0,0 +1,48 @@
+﻿using AdvenOfCode.Solvers.Year2016.Day02;
+
+namespace AdventOfCode.Solvers.Tests.Year2016.Day02;
+
+public sealed class BathroomSecuritySolverTests : AdventOfCodeTestsBase
+{
+    public override string PuzzleInputPath => @"Year2016\Day02";
+
+    private BathroomSecuritySolver CreateSolver(string[] puzzleInput)
+    {
+        return new BathroomSecuritySolver(puzzleInput);
+    }
+
+    [Theory]
+    [InlineData("example01.txt", 1985)]
+    [InlineData("input.txt", 74921)]
+    public void Part1(string inputFilePath, long expectedSolution)
+    {
+        //Arrange
+        string[] input = ReadPuzzleInput(inputFilePath);
+        BathroomSecuritySolver solver = CreateSolver(input);
+
+        //Act
+        long actualSolution = solver.SolvePart1();
+
+        //Assert
+        Assert.Equal(expectedSolution, actualSolution);
+    }
+
+
+    [Theory]
+    [InlineData("example01.txt", "5DB3")]
+    [InlineData("input.txt", "A6B35")]
+    public void Part2(string inputFilePath, string expectedSolution)
+    {
+        //Arrange
+        string[] input = ReadPuzzleInput(inputFilePath);
+        BathroomSecuritySolver solver = CreateSolver(input);
+
+        //Act
+        string actualSolution = solver.SolvePart2();
+
+        //Assert
+        Assert.Equal(expectedSolution, actualSolution);
+    }
+
+
+}
---
+++ src/AdvenOfCode.Solvers/Year2016/Day02/BathroomSecuritySolver.cs
@@ -0,0 +1,127 @@
+﻿using System.Text;
+
+namespace AdvenOfCode.Solvers.Year2016.Day02;
+
+public sealed class BathroomSecuritySolver(string[] Input)
+{
+    public long SolvePart1()
+    {
+        long result = 0;
+        int x = 1;
+        int y = 1;
+        foreach (string line in Input)
+        {
+            result *= 10;
+            FindNextPositionSimple(ref x, ref y, line);
+            result += GetDigitSimple(x, y);
+        }
+        return result;
+    }
+
+    private static long GetDigitSimple(int x, int y)
+    {
+        return 1 + x + (3 * y);
+    }
+
+    private static void FindNextPositionSimple(ref int x, ref int y, string line)
+    {
+        foreach (char c in line)
+        {
+            switch (c)
+            {
+                case 'U':
+                    if (y > 0)
+                    {
+                        y--;
+                    }
+                    break;
+                case 'D':
+                    if (y < 2)
+                    {
+                        y++;
+                    }
+                    break;
+                case 'L':
+                    if (x > 0)
+                    {
+                        x--;
+                    }
+                    break;
+                case 'R':
+                    if (x < 2)
+                    {
+                        x++;
+                    }
+
+                    break;
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+    }
+
+    public string SolvePart2()
+    {
+        StringBuilder result = new();
+        int x = 0;
+        int y = 2;
+        foreach (string line in Input)
+        {
+            FindNextPositionComplex(ref x, ref y, line);
+            char digit = GetDigitComplex(x, y);
+            _ = result.Append(digit);
+        }
+        return result.ToString();
+    }
+
+
+    private static void FindNextPositionComplex(ref int x, ref int y, string line)
+    {
+        foreach (char c in line)
+        {
+            switch (c)
+            {
+                case 'U':
+                    if (Math.Abs(x - 2) + Math.Abs(y - 2 - 1) <= 2)
+                    {
+                        y--;
+                    }
+                    break;
+                case 'D':
+                    if (Math.Abs(x - 2) + Math.Abs(y - 2 + 1) <= 2)
+                    {
+                        y++;
+                    }
+                    break;
+                case 'L':
+                    if (Math.Abs(x - 2 - 1) + Math.Abs(y - 2) <= 2)
+                    {
+                        x--;
+                    }
+                    break;
+                case 'R':
+                    if (Math.Abs(x - 2 + 1) + Math.Abs(y - 2) <= 2)
+                    {
+                        x++;
+                    }
+
+                    break;
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+    }
+
+    private static char GetDigitComplex(int x, int y)
+    {
+        return y switch
+        {
+            0 => "1"[x - 2],
+            1 => "234"[x - 1],
+            2 => "56789"[x],
+            3 => "ABC"[x - 1],
+            4 => "D"[x - 2],
+            _ => throw new InvalidOperationException(),
+        };
+    }
+}
