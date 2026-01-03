cf437374e1afed5ba59f57ac3203356eb19cd16f
PlopKonijn <plopkonijn+git@gmail.com>
2026-01-03 23:34:43 +0100

    solved day 3

---
+++ AdventOfCode.Solvers.Tests/Year2016/Day03/SquaresWithThreeSidesSolverTests.cs
@@ -0,0 +1,47 @@
+﻿using AdvenOfCode.Solvers;
+using AdvenOfCode.Solvers.Year2016.Day03;
+
+namespace AdventOfCode.Solvers.Tests.Year2016.Day03;
+
+public sealed class SquaresWithThreeSidesSolverTests : AdventOfCodeTestsBase<SquaresWithThreeSidesSolver>
+{
+    public override string PuzzleInputPath => @"Year2016\Day03";
+
+    protected override SquaresWithThreeSidesSolver CreateSolver(string[] puzzleInput)
+    {
+        return new SquaresWithThreeSidesSolver(puzzleInput);
+    }
+
+    [Theory]
+    [InlineData("example01.txt", 0)]
+    [InlineData("input.txt", 983)]
+    public override void Part1(string inputFilePath, long expectedSolution)
+    {
+        //Arrange
+        string[] input = ReadPuzzleInput(inputFilePath);
+        Solver solver = CreateSolver(input);
+
+        //Act
+        long actualSolution = solver.SolvePart1();
+
+        //Assert
+        Assert.Equal(expectedSolution, actualSolution);
+    }
+
+
+    [Theory]
+    [InlineData("example02.txt", 6)]
+    [InlineData("input.txt", 0)]
+    public override void Part2(string inputFilePath, long expectedSolution)
+    {
+        //Arrange
+        string[] input = ReadPuzzleInput(inputFilePath);
+        Solver solver = CreateSolver(input);
+
+        //Act
+        long actualSolution = solver.SolvePart2();
+
+        //Assert
+        Assert.Equal(expectedSolution, actualSolution);
+    }
+}
---
+++ src/AdvenOfCode.Solvers/Year2016/Day03/SquaresWithThreeSidesSolver.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+
+namespace AdvenOfCode.Solvers.Year2016.Day03;
+
+public sealed class SquaresWithThreeSidesSolver(string[] Input) : Solver(Input)
+{
+    public override long SolvePart1()
+    {
+        long result = 0;
+        foreach (string line in Input)
+        {
+            long[] sides = line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => long.Parse(s, CultureInfo.InvariantCulture))
+                .ToArray();
+            if (sides.Length != 3)
+            {
+                throw new InvalidOperationException();
+            }
+            if (IsValidTriangle(sides[0], sides[1], sides[2]))
+            {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    private static bool IsValidTriangle(long a, long b, long c)
+    {
+        return a + b > c && a + c > b && b + c > a;
+    }
+
+    public override long SolvePart2()
+    {
+        long result = 0;
+
+        foreach (string[] lines in Input.Chunk(3))
+        {
+            long[][] multiSides = lines.Select(line =>
+            {
+
+                return line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(s => long.Parse(s, CultureInfo.InvariantCulture))
+                    .ToArray();
+            }).ToArray();
+            for (int i = 0; i < 3; i++)
+            {
+                if (IsValidTriangle(multiSides[0][i], multiSides[1][i], multiSides[2][i]))
+                {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+}
